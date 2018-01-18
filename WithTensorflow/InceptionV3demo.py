8dc6e51cdb1b385602a98c6bc4ca1442ba1f9d86
Beans Lee <liguang456@126.com>
2018-01-18 17:00:01 +0800

    Inception V3

--- WithTensorflow/InceptionV3demo.py
+++ WithTensorflow/InceptionV3demo.py
@@ -1,10 +1,20 @@
 # _*_ coding:utf-8 _*_
 
+import math
+import time
 import tensorflow as tf
+from datetime import datetime
+
+batch_size = 32
+num_batches = 100
+height, width = 299, 299
 
 slim = tf.contrib.slim
 #  产生截断的正态分布
-tranc_normal = lambda stddev: tf.truncated_normal_initializer(0.0, stddev)
+trunc_normal = lambda stddev: tf.truncated_normal_initializer(0.0, stddev)
+
+def print_activations(t):
+    print(t.op.name, '  ', t.get_shape().as_list())
 
 # 生成网络中经常用到的函数的默认参数
 '''
@@ -17,13 +27,13 @@ def inception_v3_arg_scope(weight_decay=0.00004,
                            batch_norm_var_collection='moving_vars'):
     batch_norm_params = {
         'decay': 0.997,  # 衰减系数
-        'epilon': 0.001,
+        'epsilon': 0.001,
         'updates_collections': tf.GraphKeys.UPDATE_OPS,
         'variables_collections': {
             'beta': None,
             'gamma': None,
             'moving_mean': [batch_norm_var_collection],
-            'moving_variance':[batch_norm_var_collection]
+            'moving_variance': [batch_norm_var_collection]
         }
     }
 
@@ -40,9 +50,7 @@ def inception_v3_arg_scope(weight_decay=0.00004,
 # 定义Inception V3
 def inception_v3_base(inputs, scope=None):
     end_points = {}
-
-    with tf.variable_scope(scope, 'InceptionV3', [input]):
-
+    with tf.variable_scope(scope, 'InceptionV3', [inputs]):
         # 最开始的卷积部分
         with slim.arg_scope([slim.conv2d, slim.max_pool2d, slim.avg_pool2d],
                             stride=1, padding='VALID'):
@@ -72,6 +80,7 @@ def inception_v3_base(inputs, scope=None):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_0a_3x3')
                     branch_3 = slim.conv2d(branch_3, 32, [1, 1], scope='Conv2d_0b_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 第一个Inception模块的第二个Inception Model
             with tf.variable_scope('Mixed_5c'):
@@ -88,6 +97,7 @@ def inception_v3_base(inputs, scope=None):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_0a_3x3')
                     branch_3 = slim.conv2d(branch_3, 64, [1, 1], scope='Conv2d_0b_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 第一个Inception模块的第三个Inception Model
             with tf.variable_scope('Mixed_5d'):
@@ -104,18 +114,20 @@ def inception_v3_base(inputs, scope=None):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_0a_3x3')
                     branch_3 = slim.conv2d(branch_3, 64, [1, 1], scope='Conv2d_0b_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 第二个Inception模块的第一个Inception Model
             with tf.variable_scope('Mixed_6a'):
                 with tf.variable_scope('Branch_0'):
-                    branch_0 = slim.conv2d(net, 384, [3, 3], stride=2, padding='SAME', scope='Conv2d_1a_3x3')
+                    branch_0 = slim.conv2d(net, 384, [3, 3], stride=2, padding='VALID', scope='Conv2d_1a_3x3')
                 with tf.variable_scope('Branch_1'):
                     branch_1 = slim.conv2d(net, 64, [1, 1], scope='Conv2d_1a_1x1')
                     branch_1 = slim.conv2d(branch_1, 96, [3, 3], scope='Conv2d_1b_3x3')
                     branch_1 = slim.conv2d(branch_1, 96, [3, 3], stride=2, padding='VALID', scope='Conv2d_1c_3x3')
-                with tf.variable('Branch_2'):
-                    branch_2 = slim.max_pool2d(net, [3, 3], stride=2, padding='SAME', scope='MacPool_1a_3x3')
+                with tf.variable_scope('Branch_2'):
+                    branch_2 = slim.max_pool2d(net, [3, 3], stride=2, padding='VALID', scope='MacPool_1a_3x3')
                 net = tf.concat([branch_0, branch_1, branch_2], 3)
+                print_activations(net)
 
             # 第二个Inception模块的第二个Inception Model
             with tf.variable_scope('Mixed_6b'):
@@ -135,6 +147,7 @@ def inception_v3_base(inputs, scope=None):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_1a_3x3')
                     branch_3 = slim.conv2d(branch_3, 192, [1, 1], scope='Conv2d_1b_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 第二个Inception模块的第三个Inception Model
             with tf.variable_scope('Mixed_6c'):
@@ -154,6 +167,7 @@ def inception_v3_base(inputs, scope=None):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_1a_3x3')
                     branch_3 = slim.conv2d(branch_3, 192, [1, 1], scope='Conv2d_1b_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 第二个Inception模块的第四个Inception Model
             with tf.variable_scope('Mixed_6d'):
@@ -173,6 +187,7 @@ def inception_v3_base(inputs, scope=None):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_1a_3x3')
                     branch_3 = slim.conv2d(branch_3, 192, [1, 1], scope='Conv2d_1b_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 第二个Inception模块的第五个Inception Model
             with tf.variable_scope('Mixed_6e'):
@@ -192,6 +207,7 @@ def inception_v3_base(inputs, scope=None):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_1a_3x3')
                     branch_3 = slim.conv2d(branch_3, 192, [1, 1], scope='Conv2d_1b_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 将Mixed_6e作为Auxiliary Classifier辅助模型的分类
             end_points['Mixed_6e'] = net
@@ -200,7 +216,7 @@ def inception_v3_base(inputs, scope=None):
             with tf.variable_scope('Mixed_7a'):
                 with tf.variable_scope('Branch_0'):
                     branch_0 = slim.conv2d(net, 192, [1, 1], scope='Conv2d_2a_1x1')
-                    branch_0 = slim.conv2d(branch_0, 320, [3, 3], scope='Conv2d_2b_3x3')
+                    branch_0 = slim.conv2d(branch_0, 320, [3, 3], stride=2, padding='VALID', scope='Conv2d_2b_3x3')
                 with tf.variable_scope('Branch_1'):
                     branch_1 = slim.conv2d(net, 192, [1, 1], scope='Conv2d_2a_1x1')
                     branch_1 = slim.conv2d(branch_1, 192, [1, 7], scope='Conv2d_2b_1x7')
@@ -208,7 +224,8 @@ def inception_v3_base(inputs, scope=None):
                     branch_1 = slim.conv2d(branch_1, 192, [3, 3], stride=2, padding='VALID', scope='Conv2d_2d_3x3')
                 with tf.variable_scope('Branch_2'):
                     branch_2 = slim.max_pool2d(net, [3, 3], stride=2, padding='VALID', scope='Conv2d_2a_3x3')
-                net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                net = tf.concat([branch_0, branch_1, branch_2], 3)
+                print_activations(net)
 
             # 第三个Inception模块的第二个Inception Model
             with tf.variable_scope('Mixed_7b'):
@@ -216,21 +233,22 @@ def inception_v3_base(inputs, scope=None):
                     branch_0 = slim.conv2d(net, 320, [1, 1], scope='Conv2d_2a_1x1')
                 with tf.variable_scope('Branch_1'):
                     branch_1 = slim.conv2d(net, 384, [1, 1], scope='Conv2d_2a_1x1')
-                    branch_1 = tf.concat(
+                    branch_1 = tf.concat([
                         slim.conv2d(branch_1, 384, [1, 3], scope='Conv2d_2b_1x3'),
-                        slim.conv2d(branch_1, 384, [3, 1], scope='Conv2d_2b_3x1'), 3
+                        slim.conv2d(branch_1, 384, [3, 1], scope='Conv2d_2b_3x1')], 3
                     )
                 with tf.variable_scope('Branch_2'):
                     branch_2 = slim.conv2d(net, 448, [1, 1], scope='Conv2d_2a_1x1')
                     branch_2 = slim.conv2d(branch_2, 348, [3, 3], scope='Conv2d_2b_3x3')
-                    branch_2 = tf.concat(
+                    branch_2 = tf.concat([
                         slim.conv2d(branch_2, 384, [1, 3], scope='Conv2d_2c_1x3'),
-                        slim.conv2d(branch_2, 384, [3, 1], scope='Conv2d_2c_3x1'), 3
+                        slim.conv2d(branch_2, 384, [3, 1], scope='Conv2d_2c_3x1')], 3
                     )
                 with tf.variable_scope('Branch_3'):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_2a_3x3')
                     branch_3 = slim.conv2d(branch_3, 192, [1, 1], scope='Conv2d_2a_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 第三个Inception模块的第三个Inception Model
             with tf.variable_scope('Mixed_7c'):
@@ -238,21 +256,105 @@ def inception_v3_base(inputs, scope=None):
                     branch_0 = slim.conv2d(net, 320, [1, 1], scope='Conv2d_2a_1x1')
                 with tf.variable_scope('Branch_1'):
                     branch_1 = slim.conv2d(net, 384, [1, 1], scope='Conv2d_2a_1x1')
-                    branch_1 = tf.concat(
+                    branch_1 = tf.concat([
                         slim.conv2d(branch_1, 384, [1, 3], scope='Conv2d_2b_1x3'),
-                        slim.conv2d(branch_1, 384, [3, 1], scope='Conv2d_2b_3x1'), 3
+                        slim.conv2d(branch_1, 384, [3, 1], scope='Conv2d_2b_3x1')], 3
                     )
                 with tf.variable_scope('Branch_2'):
                     branch_2 = slim.conv2d(net, 448, [1, 1], scope='Conv2d_2a_1x1')
                     branch_2 = slim.conv2d(branch_2, 348, [3, 3], scope='Conv2d_2b_3x3')
-                    branch_2 = tf.concat(
+                    branch_2 = tf.concat([
                         slim.conv2d(branch_2, 384, [1, 3], scope='Conv2d_2c_1x3'),
-                        slim.conv2d(branch_2, 384, [3, 1], scope='Conv2d_2c_3x1'), 3
+                        slim.conv2d(branch_2, 384, [3, 1], scope='Conv2d_2c_3x1')], 3
                     )
                 with tf.variable_scope('Branch_3'):
                     branch_3 = slim.avg_pool2d(net, [3, 3], scope='AvgPool_2a_3x3')
                     branch_3 = slim.conv2d(branch_3, 192, [1, 1], scope='Conv2d_2a_1x1')
                 net = tf.concat([branch_0, branch_1, branch_2, branch_3], 3)
+                print_activations(net)
 
             # 返回Inception Model模型的输出结果
             return net, end_points
+
+# 全局平均池化、Softamx和Auxiliary Logits
+def inception_v3(inputs,
+                 num_classes=1000,
+                 is_training=True,
+                 dropout_keep_prob=0.8,
+                 prediction_fn=slim.softmax,
+                 spatial_squeeze=True,
+                 reuse=None,
+                 scope='InceptionV3'):
+    with tf.variable_scope(scope, 'InceptionV3', [inputs, num_classes], reuse=reuse) as scope:
+        with slim.arg_scope([slim.batch_norm, slim.dropout], is_training=is_training):
+            net, end_points = inception_v3_base(inputs, scope=scope)
+
+            with slim.arg_scope([slim.conv2d, slim.max_pool2d, slim.avg_pool2d], stride=1, padding='SAME'):
+
+                # 辅助分类节点
+                aux_logits = end_points['Mixed_6e']
+                with tf.variable_scope('AuxLogits'):
+                    aux_logits = slim.avg_pool2d(aux_logits, [5, 5], stride=3, padding='VALID', scope='AvgPool_1a_5x5')
+                    aux_logits = slim.conv2d(aux_logits, 128, [1, 1], scope='Conv2d_1b_1x1')
+                    aux_logits = slim.conv2d(aux_logits, 768, [5, 5], weights_initializer=trunc_normal(0.01),
+                                             padding='VALID', scope='Conv2d_2a_5x5')
+                    aux_logits = slim.conv2d(aux_logits, num_classes, [1, 1], activation_fn=None, normalizer_fn=None,
+                                             weights_initializer=trunc_normal(0.001), scope='Conv2d_2b_1x1')
+                    if spatial_squeeze:
+                        aux_logits = tf.squeeze(aux_logits, [1, 2], name='SpatialSqueeze')
+                        print_activations(aux_logits)
+                    end_points['AuxLogits'] = aux_logits
+
+                # 正常预测分类节点
+                with tf.variable_scope('Logits'):
+                    net = slim.avg_pool2d(net, [8, 8], padding='VALID', scope='AvgPool_1a_8x8')
+                    net = slim.dropout(net, keep_prob=dropout_keep_prob, scope='Dropout_1b')
+                    end_points['PredLogits'] = net
+                    logits = slim.conv2d(net, num_classes, [1, 1], activation_fn=None,
+                                         normalizer_fn=None, scope='Conv2d_1c_1x1')
+                    if spatial_squeeze:
+                        logits = tf.squeeze(logits, [1, 2], name='SpatialSqueeze')
+                        print_activations(logits)
+                    end_points['Logits'] = logits
+                    end_points['Predictions'] = prediction_fn(logits, scope='Predictions')
+
+        return logits, end_points
+
+def time_tensorflow_run(session, target, info_string):
+    num_steps_burn_in = 10
+    total_duration = 0.0
+    total_durtaion_squared = 0.0
+    for i in range(num_batches + num_steps_burn_in):
+        start_time = time.time()
+        _ = session.run(target)
+        duration = time.time() - start_time
+        if i >= num_steps_burn_in:
+            if not i % 10:
+                print('%s: step %d,  duration = %.3f' % (datetime.now(), i - num_steps_burn_in, duration))
+            total_duration += duration
+            total_durtaion_squared += duration * duration
+
+    mn = total_duration / num_batches
+    vr = total_durtaion_squared / num_batches - mn * mn
+    sd = math.sqrt(vr)
+    print('%s: %s across %d steps, %.3f +/- %.3f sec / batch' % (datetime.now(), info_string, num_batches, mn, sd))
+
+def run_benchmark():
+
+        inputs = tf.Variable(tf.random_normal([batch_size, height, width, 3], dtype=tf.float32, stddev=1e-1))
+        print(inputs)
+        with slim.arg_scope(inception_v3_arg_scope()):
+            logits, end_points = inception_v3(inputs, is_training=False)
+
+        init = tf.global_variables_initializer()
+        sess = tf.Session()
+        sess.run(init)
+        time_tensorflow_run(sess, logits, "Forward")
+
+
+if __name__ == '__main__':
+    run_benchmark()
+    #inputs = tf.Variable(tf.random_normal([batch_size, height, width, 3], dtype=tf.float32, stddev=1e-1))
+    # net, end_points = inception_v3_base(inputs)
+    # print(net)
+    # print(end_points['Mixed_6e'])
